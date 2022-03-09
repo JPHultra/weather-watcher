33c282100a7ab54f94f83e2505304839aefff9ca
Joao Gomes <joao_Pgomes@hotmail.com>
2022-03-09 14:13:54 +0000

    fd

--- src/app/weather-panel/weather-panel.component.ts
+++ src/app/weather-panel/weather-panel.component.ts
@@ -19,7 +19,7 @@ export class WeatherPanelComponent implements OnInit {
     console.log("init");
 
     this.WeatherService.get(Constants.API_VISEU_WEATHER_ENDPOINT).subscribe(res => {
-      console.log(Object.getOwnPropertyNames(res));
+      console.log(Object.getOwnPropertyNames(res.data[0]));
     });
   }
 
