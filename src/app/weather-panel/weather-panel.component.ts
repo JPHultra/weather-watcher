bc0366ef73864e1e6d713ff57139c67ccfab4def
Joao Gomes <joao_Pgomes@hotmail.com>
2022-03-09 15:23:59 +0000

    dsa

--- src/app/weather-panel/weather-panel.component.ts
+++ src/app/weather-panel/weather-panel.component.ts
@@ -20,14 +20,32 @@ export class WeatherPanelComponent implements OnInit {
   hello = "Hello"
 
   weather: any[] = [];
+  weatherType: any[] = [];
+  windClass: any[] = [];
+  precipitationClass: any[] = [];
 
   ngOnInit(): void {
-    console.log("init");
-
-
     this.WeatherService.get(Constants.API_VISEU_WEATHER_ENDPOINT).subscribe((res: any) => {
       res.data.map((entry: any) =>{
         this.weather.push(entry);
+      });
+    });
+
+    this.WeatherService.get(Constants.API_WEATHER_TYPE_ENDPOINT).subscribe((res: any) => {
+      res.data.map((entry: any) =>{
+        this.weatherType.push(entry);
+      });
+    });
+
+    this.WeatherService.get(Constants.API_WIND_CLASS_ENDPOINT).subscribe((res: any) => {
+      res.data.map((entry: any) =>{
+        this.windClass.push(entry);
+      });
+    });
+
+    this.WeatherService.get(Constants.API_PRECIPITATION_CLASS_ENDPOINT).subscribe((res: any) => {
+      res.data.map((entry: any) =>{
+        this.precipitationClass.push(entry);
       });
     });
   }
